6652d0ff5c95c584eeac58a73cc7988f5ce11715
seohyon-cho <flanique@naver.com>
2023-11-14 16:26:23 +0900

    11.14 전자시계 시간표기 변경 과제

--- js/main.js
+++ js/main.js
@@ -4,8 +4,24 @@ setInterval(() => {
 	const hr = new Date().getHours();
 	const min = new Date().getMinutes();
 	const sec = new Date().getSeconds();
+	let num = 1;
 
-	spanHr.innerText = hr;
-	spanMin.innerText = min;
-	spanSec.innerText = sec;
+	// spanHr.innerText = hr;
+	if (hr > 12) {
+		spanHr.innerText = '0' + (hr - 12);
+	} else {
+		spanHr.innerText = hr;
+	}
+	// spanMin.innerText = min;
+	if (min < 10) {
+		spanMin.innerText = '0' + min;
+	} else {
+		spanMin.innerText = min;
+	}
+	// spanSec.innerText = sec;
+	if (sec < 10) {
+		spanSec.innerText = '0' + sec;
+	} else {
+		spanSec.innerText = sec;
+	}
 }, 1000);
