d4f26715ab3e2b0e2a191fa8c7e2eabbdaf39260
seohyon-cho <flanique@naver.com>
2023-11-14 13:09:10 +0900

    전역변수의 개념에 대한 비유

--- js/main.js
+++ js/main.js
@@ -2,10 +2,12 @@ const h1 = document.querySelector('h1');
 
 const [btnStart, btnStop] = document.querySelectorAll('button');
 
+// 전역변수 : 우주에 떠있는 인공위성 (중계기)
 let num = 0;
 let timer = null;
 
 timer = setInterval(() => {
+	// A지역에서 변경요청
 	h1.innerText = num++;
 }, 1000);
 
@@ -14,6 +16,7 @@ btnStop.addEventListener('click', () => {
 });
 btnStart.addEventListener('click', () => {
 	timer = setInterval(() => {
+		// B지역에서 변경요청
 		h1.innerText = num++;
 	}, 1000);
 });
